ecb5886ceb0b984deae4ac83fdf7e21033cc8816
Martina350 <martinacamilaleon@gmail.com>
2024-08-08 11:42:36 -0500

    mostrarTextoEnCaja implementado

--- utilitarios/saludo.js
+++ utilitarios/saludo.js
@@ -3,8 +3,33 @@ saludar=function(){
     let apellido=recuperarTexto("txtApellido");
     let edad = recuperarInt("txtEdad");
     let estatura = recuperarFloat("txtEstatura");
+
+    let mensajeBienvenida="Bienvenido "+nombre+" "+apellido;
+    mostrarTexto("lblResultado",mensajeBienvenida);
+
+    mostrarImagen("imgSaludo","./imagenes/meme.gif");
+
+    mostrarTextoEnCaja("txtNombre","");
 }
     
+mostrarImagen=function(idComponente,rutaImagen){
+    let componente;
+    componente=document.getElementById(idComponente);
+    componente.src = rutaImagen;
+}
+
+mostrarTexto=function(idComponente,mensaje){
+    let componente;
+    componente=document.getElementById(idComponente);
+    componente.innerText=mensaje;
+}
+
+mostrarTextoEnCaja=function(idComponente,mensaje){
+    let componente;
+    componente=document.getElementById(idComponente);
+    componente.value=mensaje;
+}
+
 recuperarTexto=function(idComponente){
     let componente;
     let valorIngresado;
