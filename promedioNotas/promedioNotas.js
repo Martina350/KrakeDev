dd70d3e2758ad0d04fa5cfcc7ea8f05ce9d573f5
Martina350 <martinacamilaleon@gmail.com>
2024-08-11 22:25:29 -0500

    TODO FUNCIONA SE AGREGAN GIFS

--- promedioNotas/promedioNotas.js
+++ promedioNotas/promedioNotas.js
@@ -4,4 +4,10 @@ calcularPromedioNotas=function(){
     let n3=recuperarFlotante("txtN3")
     let promedio = calcularPromedio(n1,n2,n3);
     cambiarTexto("resultadoPromedio", promedio);
+
+    if (promedio>7){
+        cambiarImagen("imgPromedio","./exito.gif")
+    }else{
+        cambiarImagen("imgPromedio","./fracaso.gif")
+    }
 }
