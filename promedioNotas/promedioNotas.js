dab916606f2459e36bfceb67125f4592c3c9dbdd
Martina350 <martinacamilaleon@gmail.com>
2024-08-11 21:54:28 -0500

    FUNCIONA CALCULAR PROMEDIO NOTAS

---
+++ promedioNotas/promedioNotas.js
@@ -0,0 +1,7 @@
+calcularPromedioNotas=function(){
+    let n1=recuperarFlotante("txtN1")
+    let n2=recuperarFlotante("txtN2")
+    let n3=recuperarFlotante("txtN3")
+    let promedio = calcularPromedio(n1,n2,n3);
+    cambiarTexto("resultadoPromedio", promedio);
+}
---
+++ promedioNotas/serviciosPromedioNotas.js
@@ -0,0 +1,7 @@
+calcularPromedio=function(n1,n2,n3){
+    let sumarTresNumeros;
+    let promedio;
+    sumarTresNumeros=n1+n2+n3;
+    promedio=sumarTresNumeros/3;
+    return promedio.toFixed(2);
+}
