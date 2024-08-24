42831c0844d7e7d328b8dafed4aaff55c16ad201
Martina350 <martinacamilaleon@gmail.com>
2024-08-23 23:53:24 -0500

    se crean las funciones validarPassword y ejecutarValidacion falta corregir errores

---
+++ password/password.js
@@ -0,0 +1,60 @@
+ejecutarValidacion=function(){
+    let password;
+    let resultadoValidacion;
+    password=recuperarTexto("txtPassword")
+    resultadoValidacion=validarPassword(password);
+    if(resultadoValidacion==""){
+        mostrarTexto("lblResultado", "PASSWORD CORRECTO");
+    } else {
+        mostrarTexto("lblResultado", resultadoValidacion);
+    }
+}
+
+
+validarPassword=function(password){
+
+    let existeError = false;
+    if(password.length < 8 || password.length > 16){
+        mostrarTexto("lblErrorLongitud","LA CONTRASEÑA DEBE TENER ENTRE 8 Y 16 CARACTERES")
+        existeError=true;
+    }else{
+        mostrarTexto("lblErrorLongitud"," ");
+    }
+
+    let tieneMayuscula = false;
+    let tieneDigito = false;
+    let tieneCaracterEspecial = false;
+
+    for(let i = 0; i < password.length; i++){
+        let caracter=password.charAt(i);
+        if(esMayuscula(caracter)){
+            tieneMayuscula=true;
+        }else if(esDigito(caracter)){
+            tieneDigito=true;
+        }else if(esCaracterEspecial(caracter)){
+            tieneCaracterEspecial=true;
+        }
+    }
+
+    if(!tieneMayuscula){
+        mostrarTexto("lblErrorMayuscula","LA CONTRASEÑA DEBE TENER AL MENOS UNA LETRA MAYÚSCULA");
+        existeError = true;
+    }else{
+        mostrarTexto("lblErrorMayuscula"," ");
+    }
+
+    if(!tieneDigito){
+        mostrarTexto("lblErrorDigito","LA CONTRASEÑA DEBE TENER AL MENOS UN DÍGITO");
+        existeError = true;
+    }else{
+        mostrarTexto("lblErrorDigito"," ");
+    }
+
+    if(!tieneCaracterEspecial){
+        mostrarTexto("lblErrorEspecial","LA CONTRASEÑA DEBE TENER AL MENOS UN CARÁCTER ESPECIAL (*, -, _)");
+        existeError = true;
+    }else{
+        mostrarTexto("lblErrorEspecial"," ");
+    }
+    return !existeError; 
+}
---
+++ password/utilitariosCadenas.js
@@ -0,0 +1,35 @@
+esMayuscula=function(caracter){
+    let codigoAscii=caracter.charCodeAt(0);
+    if(codigoAscii>=65 && codigoAscii<=90){
+        return true;
+    }else{
+        return false
+    }
+}
+
+esDigito=function(caracter){
+    let codigoAscii=caracter.charCodeAt(0);
+    if(codigoAscii>=48 && codigoAscii<=57){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+esGuion=function(caracter){
+    let codigoAscii=caracter.charCodeAt(0);
+    if(codigoAscii == 45){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+esCaracterEspecial=function(caracter){
+    let codigoAscii=caracter.charCodeAt(0);
+    if (codigoAscii==42 ||  codigoAscii==45 ||  codigoAscii==95){  
+        return true;
+    }else{
+        return false;
+    }
+}
