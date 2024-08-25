820a9c7a2467b16ec1eda22c4c940ce6b27928fc
Martina350 <martinacamilaleon@gmail.com>
2024-08-25 16:11:33 -0500

    SE AGREGAN TABLAS EN HTML Y JS

--- arreglos/ejercicios1.js
+++ arreglos/ejercicios1.js
@@ -21,6 +21,7 @@ probarAgregar=function(){
 
 agregarNota=function(nota){
     notas.push(nota);
+    mostrarNotas();
 }
 
 calcularPromedio=function(){
@@ -37,4 +38,26 @@ ejecutarPromedio=function(){
     let totalPromedio=calcularPromedio();
     totalPromedio=totalPromedio.toFixed(2);
     mostrarTexto("lblPromedio",totalPromedio);
+}
+
+generarTabla=function(){
+    let contenidoTabla="";
+    let cmpTabla=document.getElementById("divTabla");
+    contenidoTabla+="<table><tr><td>UNO<td/><tr/>"+
+    "<tr><td>DOS<td/><tr/></table>";
+    cmpTabla.innerHTML=contenidoTabla;
+}
+
+mostrarNotas=function(){
+    let miNota;
+    let cmpTabla=document.getElementById("divTabla");
+    let contenidoTabla="<table><tr><th>NOTA</th></tr>";
+    for(let i=0;i<notas.length;i++){
+        miNota=notas[i];
+        contenidoTabla+="<tr><td>";
+        contenidoTabla+=miNota;
+        contenidoTabla+="</tr></td>"
+    }
+    contenidoTabla+="</table>";
+    cmpTabla.innerHTML=contenidoTabla;
 }
