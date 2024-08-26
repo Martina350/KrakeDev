fbb4f5f3fef302305b2d3d64eda0edcda3ca1fe2
Martina350 <martinacamilaleon@gmail.com>
2024-08-26 14:49:33 -0500

    FUNCIONAMIENTO DE LA PANTALLA DE INGRESO,BUSQUEDA Y  MODIFICACION DE CLIENTES

--- objetos/clientes.js
+++ objetos/clientes.js
@@ -1,8 +1,83 @@
 let clientes=[
-    {cedula:"174563289",nombre:"JUAN",edad:"20"},
-    {cedula:"178521477",nombre:"MARIO",edad:"50"},
-    {cedula:"176985324",nombre:"PEPE",edad:"22"}
+    {cedula:"1745632891",nombre:"JUAN",edad:"20"},
+    {cedula:"1785214775",nombre:"MARIO",edad:"50"},
+    {cedula:"1769853243",nombre:"PEPE",edad:"22"}
 ];
+
+guardarCambios=function(){
+    let valorCedula=recuperarTexto("txtCedula");
+    let valorNombre=recuperarTexto("txtNombre");
+    let valorEdad=recuperarFloat("txtEdad");
+
+    let datosCliente={};
+    datosCliente.cedula=valorCedula;
+    datosCliente.nombre=valorNombre;
+    datosCliente.edad=valorEdad;
+
+    modificarCliente(datosCliente);
+    
+    mostrarClientes();
+}
+
+modificarCliente=function(cliente){
+    let clienteEncontrado=buscarCliente(cliente.cedula);
+    if(clienteEncontrado!=null){
+        clienteEncontrado.nombre=cliente.nombre;
+        clienteEncontrado.edad=cliente.edad;
+    }
+}
+
+ejecutarBusqueda=function(){
+    let valorCedula=recuperarTexto("txtCedulaBusqueda");
+    let cliente=buscarCliente(valorCedula);
+    if(cliente==null){
+        alert("CLIENTE NO ENCONTRADO");
+    }else{
+        mostrarTextoEnCaja("txtCedula",cliente.cedula);
+        mostrarTextoEnCaja("txtNombre",cliente.nombre);
+        mostrarTextoEnCaja("txtEdad",cliente.edad);
+    }
+}
+
+crearCliente=function(){
+    let valorCedula=recuperarTexto("txtCedula");
+    let valorNombre=recuperarTexto("txtNombre");
+    let valorEdad=recuperarFloat("txtEdad");
+
+    let nuevoCliente={};
+    nuevoCliente.cedula=valorCedula;
+    nuevoCliente.nombre=valorNombre;
+    nuevoCliente.edad=valorEdad;
+
+    agregarCliente(nuevoCliente);
+
+}
+
+agregarCliente=function(cliente){
+    let resultado;
+    resultado=buscarCliente(cliente.cedula);
+    if(resultado==null){
+        clientes.push(cliente);
+        alert("CLIENTE AGREGADO");
+        mostrarClientes();
+    }else{
+        alert("YA EXISTE UN CLIENTE CON LA CEDULA: "+cliente.cedula);
+    }
+    
+}
+
+buscarCliente=function(cedula){
+    let elementoCliente;
+    let clienteEncontrado=null;
+    for(let i=0;i<clientes.length;i++){
+        elementoCliente=clientes[i];
+        if(elementoCliente.cedula==cedula){
+            clienteEncontrado=elementoCliente;
+            break;
+        }
+    }
+    return clienteEncontrado;
+}
 
 mostrarClientes=function(){
     let cmpTabla=document.getElementById("tablaClientes");
