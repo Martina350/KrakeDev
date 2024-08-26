d3d189c79c60b202e4fc573221f2cd8d0568aaba
Martina350 <martinacamilaleon@gmail.com>
2024-08-26 18:28:45 -0500

    SE AGREGA TABLA Y ESTILOS

--- roles/roles.js
+++ roles/roles.js
@@ -1,12 +1,14 @@
 let empleados = [
     {cedula:"1714616123",nombre:"John",apellido:"Cena",sueldo:500.0},
-    {cedula:"0914632123",nombre:"Luisa",apellido:"Gonzalez",sueldo:900.0}
+    {cedula:"0914632123",nombre:"Luisa",apellido:"Gonzalez",sueldo:900.0},
+    {cedula:"1748592631",nombre:"Monserrath",apellido:"Teran",sueldo:300.0}
 ]
 
 mostrarOpcionEmpleado=function(){
     mostrarComponente("divEmpleado");
     ocultarComponente("divRol");
     ocultarComponente("divResumen");
+    mostrarEmpleados();
 }
 
 mostrarOpcionRol=function(){
@@ -19,4 +21,26 @@ mostrarOpcionResumen=function(){
     mostrarComponente("divResumen");
     ocultarComponente("divRol");
     ocultarComponente("divEmpleado");
+}
+
+mostrarEmpleados=function(){
+    let cmpTabla=document.getElementById("tablaEmpleados");
+    let contenidoTabla=`<table id="tabla_empleados"><tr>
+    <th>CEDULA</th>
+    <th>NOMBRE</th>
+    <th>APELLIDO</th>
+    <th>SUELDO</th>
+    </tr>`
+    let elementoEmpleados;
+    for(let i=0;i<empleados.length;i++){
+        elementoEmpleados=empleados[i];
+        contenidoTabla+=
+        "<tr><td>"+elementoEmpleados.cedula+"</td>"
+        +"<td>"+elementoEmpleados.nombre+"</td>"
+        +"<td>"+elementoEmpleados.apellido+"</td>"
+        +"<td>"+elementoEmpleados.sueldo+"</td>"
+        +"</tr>";
+    }
+    contenidoTabla+="</table>"
+    cmpTabla.innerHTML=contenidoTabla;
 }
